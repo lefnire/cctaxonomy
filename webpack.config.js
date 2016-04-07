1645003746501383dd5cc5fcd45bddacea30db3d
Tyler Renelle <tylerrenelle@gmail.com>
2016-04-07 08:23:46 -0400

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const PROD = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { loader: 'babel', exclude: /node_modules/ },
+      { test: /\.css$/, loader: "style!css" }, // "style-loader!css-loader?importLoaders=1"
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader:  'url-loader' } //'url-loader?limit=100000'
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: PROD ? [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ] : [],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
